e917abd7acffe1671dce7724392fed2f15b619ca
Wilfrido Bonett <wbonett10@gmail.com>
2020-04-28 21:11:21 -0500

    Refactor Timer component as statefull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { CountdownComponent } from './countdown/countdown.component';
+import { AlertViewComponent } from './alert-view/alert-view.component';
+import { DisplayComponent } from './display/display.component';
+import { SimpleAlertViewComponent } from './simple-alert-view/simple-alert-view.component';
+import { TabComponent } from './tab/tab.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProgressBarComponent,
-    CountdownComponent
+    AlertViewComponent,
+    DisplayComponent,
+    SimpleAlertViewComponent,
+    TabComponent,
+    TabsComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss']
+})
+export class TimerComponent implements OnInit, OnDestroy {
+
+  private countdownTimerRef: any = null;
+  public countdown: number = 0;
+  public paused: boolean = true;
+
+  @Input() init: number = 20;
+  @Output() onComplete = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(this.init && this.init > 0) {
+      this.countdown = this.init;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.clearTimeout();
+  }
+
+  restartCountdown() {
+    if (this.init && this.init > 0) {
+      this.paused = true;
+      this.clearTimeout();
+      this.countdown = this.init;
+    }
+  }
+
+  toogleCountdown() {
+    this.paused = !this.paused;
+
+    if(this.paused == false) {
+      this.doCountdown();
+    }else {
+      this.clearTimeout();
+    }
+  }
+
+  private doCountdown() {
+    this.countdownTimerRef = setTimeout(() => {
+      this.countdown = this.countdown - 1;
+      this.processCountdown();
+    }, 1000);
+  }
+
+  private processCountdown() {
+    if (this.countdown == 0) {
+      this.onComplete.emit();
+      console.log("--countdown end--");
+    }
+    else {
+      this.doCountdown();
+    }
+  }
+
+  private clearTimeout() {
+    if (this.countdownTimerRef) {
+      clearTimeout(this.countdownTimerRef);
+      this.countdownTimerRef = null;
+    }
+  }
+
+}
