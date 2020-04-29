1f9e59d96aa0d30cecaee65e76bf3ae4d490ed84
Wilfrido Bonett <wbonett10@gmail.com>
2020-04-29 10:22:56 -0500

    I add Modal and ng content on alert view

--- src/app/alert-view/alert-view.component.ts
+++ src/app/alert-view/alert-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-alert-view',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert-view.component.scss']
 })
 export class AlertViewComponent implements OnInit {
+
+  @Output() onCancel = new EventEmitter<void>();
+  @Output() onSubmit = new EventEmitter<number>();
 
   constructor() { }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  public isAddTimerVisible: boolean = false;
+  public time: number = 0;
+
   constructor() { }
 
   logCountdownEnd() {
     console.log("countdown has finished");
+  }
+
+  public showAddTimer() {
+    this.isAddTimerVisible = true;
+  }
+
+  public hiddenAddTimer() {
+    this.isAddTimerVisible = false;
   }
 }
